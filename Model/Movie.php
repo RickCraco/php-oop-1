8b6ae05bb8a792ae8fa5cdeda6cab29a1686822a
RickCraco <riccardocracolici43@gmail.com>
2023-12-04 15:34:23 +0100

    aggiunto metodo per la stampa del voto come stellina

--- Model/Movie.php
+++ Model/Movie.php
@@ -27,6 +27,16 @@ class Movie{
 
         include __DIR__."/../Views/card.php";
     }
+
+    public function getVote(){
+        $vote = ceil($this->vote / 2);
+        $template = "<p class= 'm-0'>";
+        for($n=1; $n<=$vote; $n++){
+            $template .= $n <= $vote ? '<i class="fa-solid fa-star"></i>' : '<i class="fa-regular fa-star"></i>';
+        }
+        $template .= "</p>";
+        return $template;
+    }
 }
 
 $movieString = file_get_contents(__DIR__.'/movie_db.json');
