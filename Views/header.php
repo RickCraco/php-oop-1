9cd92935ec78c78305eea3d7623fc1959a312d9d
RickCraco <riccardocracolici43@gmail.com>
2023-12-04 15:05:58 +0100

    aggiunto header della pagina

---
+++ Views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>OOP Shop</h1>
+            <ul>
+                <li><a href="index.php"></a>Movies</li>
+                <li><a href="book.php"></a>Books</li>
+            </ul>
+        </div>
+    </header>
+    <main>
+
+    
